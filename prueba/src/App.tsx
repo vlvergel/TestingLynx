be483388680e4fa775ea26ae7be0cc1fa4e7a43f
Valentina Vergel <vvergelv@ufpso.edu.co>
2025-04-05 09:54:27 -0500

    first commit

---
+++ prueba/src/App.tsx
@@ -0,0 +1,22 @@
+import { useCallback, useEffect, useState } from '@lynx-js/react'
+import Card from '@mui/material/Card';
+import './App.css'
+import { CardActionArea, CardContent, CardMedia, Typography } from '@mui/material';
+
+
+export function App() {
+
+
+  return (
+    <>
+      <view>
+        <text>hello world !!!</text>
+
+        <Typography gutterBottom variant="h5" component={Text}>
+          Lizard
+        </Typography>
+
+      </view>
+    </>
+  )
+}
